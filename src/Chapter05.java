8448bf826b7bca374f050dc53e55976eb96b2351
abyes <abyes@DESKTOP-O7KGHET>
2022-12-30 12:04:56 +0900

    Lesson2 Chapter5確認問題

---
+++ src/Chapter05.java
@@ -0,0 +1,13 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		String str1 = "suzuki akiyoshi";
+		System.out.println(str1);
+		
+		str1 = "Good";
+		System.out.println(str1);
+		
+		final int num1 = 87;
+		System.out.println(num1);
+	}
+}
