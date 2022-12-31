cbb0dff7c572c05f19471d025851471cc6dc2912
abyes <abyes@DESKTOP-O7KGHET>
2022-12-31 15:12:40 +0900

    Lesson2 Chapter06 問題

---
+++ src/Chapter06.java
@@ -0,0 +1,27 @@
+
+public class Chapter06 {
+	public static void main(String[] args) {
+		
+		long lg = 10000000000L;
+		System.out.println(lg);
+		
+		double db = 3.14;
+		System.out.println(db);
+		
+		boolean bn = true;
+		System.out.println(bn);
+		
+		Integer ir = 100;
+		System.out.println(ir);
+		
+		Float ft = 1.4142f;
+		System.out.println(ft);
+		
+		Character chr = 'う';
+		System.out.println(chr);
+		
+		int num[] = {0, 10, 20, 30 ,40};
+		System.out.println(num[3]);
+		
+	}
+}
